5566c8cd9750ce1bbb8d520045ffd592966c9d1f
aeshikaz <aeshikasingh@gmail.com>
2026-03-10 07:01:57 +0530

    update

--- app.py
+++ app.py
@@ -7,21 +7,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from pytorch_grad_cam import GradCAM
-from pytorch_grad_cam.utils.image import show_cam_on_image
 
-st.set_page_config(
-    page_title="Brain Tumor Detection",
-    layout="centered"
-)
+st.set_page_config(page_title="Brain Tumor Detection", layout="centered")
 
 MODEL_PATH = "final_brain_tumor_model.pth"
 
-class_names = ['glioma', 'meningioma', 'notumor', 'pituitary']
+class_names = ['glioma','meningioma','notumor','pituitary']
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-# Load model
 @st.cache_resource
 def load_model():
     model = models.resnet50(weights=None)
@@ -34,17 +29,13 @@ def load_model():
 
 model = load_model()
 
-# Target layer for Grad-CAM
 target_layers = [model.layer4[-1]]
 
-# Image preprocessing
 transform = transforms.Compose([
-    transforms.Resize((224, 224)),
+    transforms.Resize((224,224)),
     transforms.ToTensor(),
-    transforms.Normalize(
-        [0.485, 0.456, 0.406],
-        [0.229, 0.224, 0.225]
-    )
+    transforms.Normalize([0.485,0.456,0.406],
+                         [0.229,0.224,0.225])
 ])
 
 st.title("Brain Tumor Detection")
@@ -54,30 +45,24 @@ uploaded_file = st.file_uploader("Upload MRI Image")
 if uploaded_file:
 
     image = Image.open(uploaded_file).convert("RGB")
-
     st.image(image, caption="Uploaded MRI", width=300)
 
-    # Preprocess image
     input_tensor = transform(image).unsqueeze(0).to(device)
 
-    # Prediction
     with torch.no_grad():
         outputs = model(input_tensor)
         probs = torch.softmax(outputs, dim=1)
-        confidence, pred = torch.max(probs, 1)
+        confidence, pred = torch.max(probs,1)
 
     st.success(f"Prediction: {class_names[pred.item()]}")
     st.metric("Confidence", f"{confidence.item()*100:.2f}%")
 
-    # Prepare image for GradCAM
-    img_np = np.array(image.resize((224, 224))).astype("float32") / 255.0
+    img_np = np.array(image.resize((224,224))).astype("float32") / 255.0
 
-    # Grad-CAM
     cam = GradCAM(model=model, target_layers=target_layers)
 
     grayscale_cam = cam(input_tensor=input_tensor)[0]
 
-    # Plot heatmap
     fig, ax = plt.subplots()
 
     ax.imshow(img_np)
