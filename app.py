f99253b3bab6ce47935afdbf0dbeef23d3c0d0ba
aeshikaz <aeshikasingh@gmail.com>
2026-03-10 06:33:14 +0530

    mock cv2

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
+import sys
+import types
+
+# Create a dummy cv2 module to avoid OpenCV dependency
+sys.modules['cv2'] = types.ModuleType('cv2')
 
 from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.image import show_cam_on_image
